5131bea16a87c7ef157e6bd001d6b1496eb37868
Gandhi T <t.gandhi@diacritech.in>
2017-04-24 14:54:23 +0530

    node opening

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var http = require('http');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(8081, function () {
+  console.log('Example app listening on port 8081!')
+})
