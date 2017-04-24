037c20825d131fbda8ce285098203cce28e72e73
Gandhi T <t.gandhi@diacritech.in>
2017-04-24 15:14:55 +0530

    node opening

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var http = require('http');
 var app = express();
-
+var portx = process.env.PORT;  // This is my addition
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
-
-app.listen(8081, function () {
-  console.log('Example app listening on port 8081!')
+app.set("port", portx);  // to here
+app.listen(app.get("port"), function () {
+  console.log('Example app listening on port '+app.get("port")+'!')
 })
